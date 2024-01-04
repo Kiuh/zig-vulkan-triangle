433613cc9551fe03ff17c19d59b5b257c1025145
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-01-04 22:38:27 +0300

    Add window showing

---
+++ build.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const glfw = @import("mach_glfw");
+
+pub fn build(b: *std.Build) void {
+    // Define
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const vulkan_dep = b.dependency("vulkan", .{});
+    const vulkan_mod = vulkan_dep.module("vulkan-zig-generated");
+
+    const glfw_dep = b.dependency("mach_glfw", .{ .target = target, .optimize = optimize });
+    const glfw_mod = glfw_dep.module("mach-glfw");
+
+    // Construct exe
+    const exe = b.addExecutable(.{
+        .name = "zig-vulkan-triangle",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.main_pkg_path = .{ .path = "." };
+    try glfw.link(b, exe);
+    exe.addModule("vulkan", vulkan_mod);
+    exe.addModule("glfw", glfw_mod);
+
+    // Install Artifact
+    b.installArtifact(exe);
+
+    // Addictional run command
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const vk = @import("vulkan");
+const glfw = @import("glfw");
+
+const app_name = "mach-glfw + vulkan-zig = triangle";
+
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    var extent = vk.Extent2D{ .width = 800, .height = 600 };
+
+    const window = glfw.Window.create(extent.width, extent.height, app_name, null, null, .{
+        .client_api = .no_api,
+    }) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+    }
+}
