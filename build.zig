2cf2d6ffe45578f93a05dac5be281a315aeee663
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-08-11 01:18:52 +0300

    Link necessary libraries

--- build.zig
+++ build.zig
@@ -4,33 +4,37 @@ pub fn build(b: *std.Build) void {
     // Define
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const vulkan_registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml");
 
-    // Construct exe
+    // Zglfw
+    const zglfw_dep = b.dependency("zglfw", .{});
+
+    // Vulkan-generator
+    const vk_gen = b.dependency("vulkan_zig", .{}).artifact("vulkan-zig-generator");
+    const vk_generate_cmd = b.addRunArtifact(vk_gen);
+    vk_generate_cmd.addFileArg(vulkan_registry);
+    const vulkan_zig = b.addModule("vulkan-zig", .{
+        .root_source_file = vk_generate_cmd.addOutputFileArg("vk.zig"),
+    });
+
+    // Main program exe
     const exe = b.addExecutable(.{
         .name = "zig-vulkan-triangle",
-        .root_source_file = b.path("main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const vkzig_dep = b.dependency("vulkan_zig", .{
-        .registry = @as([]const u8, b.pathFromRoot("C://Vulkan//1.3.290.0//share//vulkan//registry//vk.xml")),
-    });
-    const vkzig_bindings = vkzig_dep.module("vulkan-zig");
-    exe.addModule("vulkan", vkzig_bindings);
-
-    // Install Artifact
+    exe.root_module.addImport("zglfw", zglfw_dep.module("glfw"));
+    exe.root_module.addImport("vulkan", vulkan_zig);
     b.installArtifact(exe);
 
-    // Addictional run command
+    // Run command
     const run_exe = b.addRunArtifact(exe);
-
     run_exe.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_exe.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_exe.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,10 @@
 const std = @import("std");
 const vk = @import("vulkan");
+const glfw = @import("zglfw");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
+    try stdout.print("Hello, world!\n", .{});
+    try stdout.print("VK {}\n", .{vk.API_VERSION_1_0});
+    try stdout.print("GLFW {}\n", .{glfw.Arrow});
 }
